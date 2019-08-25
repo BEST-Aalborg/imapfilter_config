781eeecc637a5e404f36e56faf0b8c5357068b21
Dennis Værum <6872940+dvaerum@users.noreply.github.com>
2019-08-25 10:10:35 +0200

    Add the email it-id@best.eu.org to the IT folder

--- config.lua
+++ config.lua
@@ -141,6 +141,7 @@ function sorting(account, config)
              any_recipient(account[BEST_DIR], 'itc@best.eu.org') + 
              any_recipient(account[BEST_DIR], 'itid@BEST.eu.org') + 
              any_recipient(account[BEST_DIR], 'itdept@best.eu.org') + 
+             any_recipient(account[BEST_DIR], 'it-id@best.eu.org') + 
              any_recipient(account[BEST_DIR], 'helpdesk-members@best.eu.org') + 
              any_recipient(account[BEST_DIR], 'it-department@best.eu.org')
     result:move_messages(account[string.format("%s/IT", BEST_DIR)])
